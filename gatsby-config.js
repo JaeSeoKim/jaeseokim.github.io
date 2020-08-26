7bad3af947d94da82f897e71e33dec95fb36bb9f
JaeseoKim <devjaeseo@gmail.com>
2020-08-27 00:51:50 +0900

    :fire: remove dummy

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/pages/`,
-        name: "pages",
+        path: `${__dirname}/content/posts/`,
+        name: "posts",
       },
     },
     {
