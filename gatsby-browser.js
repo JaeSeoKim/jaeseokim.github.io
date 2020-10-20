6f1fb81314b9f977282027488dd6659fbd1e1e40
JaeSeoKim <devjaeseo@gmail.com>
2020-10-20 21:37:30 +0900

    :wrench: add onServiceWorkerUpdateReady

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -103,12 +103,12 @@ const Wrapper = ({ element }) => {
 
 export const wrapRootElement = ({ element }) => <Wrapper element={element} />
 
-// export const onServiceWorkerUpdateReady = () => {
-//   const answer = window.confirm(
-//     `This application has been updated. ` +
-//       `Reload to display the latest version?`
-//   )
-//   if (answer === true) {
-//     window.location.reload()
-//   }
-// }
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
